147caa2642305ff8192baca1a7e1529274ae419b
sebtn <sebtn@users.noreply.github.com>
2016-09-05 23:19:31 -0400

    Update factorlize.js

--- factorlize.js
+++ factorlize.js
@@ -7,3 +7,8 @@ function factorialize(num) {
 }
 
 factorialize(5);
+
+factorialize(5) should return 120.
+factorialize(10) should return 3628800.
+factorialize(20) should return 2432902008176640000.
+factorialize(0) should return 1.
