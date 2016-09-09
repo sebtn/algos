a0d085eca885ecfe2756fac24b3248ca791c1e26
sebtn <sebtn@users.noreply.github.com>
2016-09-08 23:24:33 -0400

    Create largestNumberInArray.js

---
+++ largestNumberInArray.js
@@ -0,0 +1,18 @@
+function largestOfFour(arr) {
+  // You can do this!
+  var newArr = [];  
+  var max = [];
+  for (var  i= 0; i < arr.length; i++) { 
+    newArr = arr[i];
+    var largest = Math.max.apply(Math, newArr);
+    max.push(largest);
+ 
+  }  
+    return max;
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]) should return an array.
+largestOfFour([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]) should return [27,5,39,1001].
+largestOfFour([[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]]) should return [9, 35, 97, 1000000].
+
